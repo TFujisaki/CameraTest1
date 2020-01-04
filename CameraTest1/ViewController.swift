5907e2e92cf19ab6851a5551197b867ea51912cf
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2020-01-05 00:42:52 +0900

    Initial Commit

---
+++ CameraTest1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraTest1
+//
+//  Created by 藤崎達也 on 2020/01/05.
+//  Copyright © 2020 藤崎達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
